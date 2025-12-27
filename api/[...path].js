f1b318aebe4bd5f158e660162f007274d71b5b05
unknown <sakethreddyjulukuntla@gmail.com>
2025-12-27 17:13:16 +0530

    Vercel API: catch-all handler prefixes /api before forwarding to Express; rewrites to /api/[...path].js

--- api/[...path].js
+++ api/[...path].js
@@ -1,5 +1,13 @@
 const app = require('./server');
 
 module.exports = (req, res) => {
+  // Ensure Express receives paths with '/api' prefix so routes match
+  if (!req.url.startsWith('/api')) {
+    req.url = '/api' + (req.url.startsWith('/') ? req.url : '/' + req.url);
+  }
+  return app(req, res);
+};const app = require('./server');
+
+module.exports = (req, res) => {
   return app(req, res);
 };
--- api/index.js
+++
@@ -1,7 +0,0 @@
-const app = require('./server');
-
-module.exports = (req, res) => {
-  return app(req, res);
-};// Export the Express app directly as a Vercel function handler
-const app = require('./server');
-module.exports = app;
